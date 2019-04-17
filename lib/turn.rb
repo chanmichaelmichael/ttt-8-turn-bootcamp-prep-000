e88e080f3965ec278025c1cbd2300d561d5f5f83
Michael Michael <michael.ta.chan@gmail.com>
2019-04-17 08:30:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def display_board(board)
 end
 
 def valid_move?(board, i)
-  if !i.between?(0,8) || board[i] == "" || board[i] == " "
+  if !i.between?(0,8) && (board[i] == "" || board[i] == " ")
     FALSE
   else
     TRUE
